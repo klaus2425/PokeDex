a56a9c48ad8049ea157a5dd80ba70316fc0f5f6a
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-06-04 00:46:24 +0800

    Create 404 page

---
+++ src/Pages/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center bg-red-500 w-svw h-svh">
+      <span className="font-bold text-white text-5xl">404 NOT FOUND</span>
+    </div>
+  )
+}
+
+export default NotFound
--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import { lazy } from "react";
+import NotFound from "./Pages/NotFound";
 
 const Home = lazy(() => import("./Pages/Home"))
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
-  }
+  },
+  {
+    path: '*',
+    element: <NotFound />
+}
 ])
 
 export default router;
