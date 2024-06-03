205f1619dd28c824314b4db1ad977695731b5bed
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-06-03 21:40:00 +0800

    Add design on navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,10 +1,21 @@
 
 const Navbar = () => {
   return (
-    <div className="w-full flex items-center border-b-8 border-b-black gap-2 bg-red-500 h-fit p-3 px-3">
-      <img className="w-14" src={'/pokeball.svg'} alt="" />
-      <div className="font-bold text-white text-3xl">Pokédex</div>
+    <div className="w-full flex flex-col items-center ">
+      <div className="w-full flex items-center border-b-[12px] border-b-black gap-2 bg-red-500 h-fit p-3 px-3">
+        <img className="w-14" src={'/pokeball.svg'} alt="" />
+        <div className="font-bold text-white text-3xl">Pokédex</div>
+
+      </div>
+      <div className="h-20 w-20 -mt-12 bg-black 
+            rounded-full flex justify-center items-center">
+        <div className="h-14 w-14 bg-white 
+            rounded-full ">
+        </div>
+
+      </div>
     </div>
+
   )
 }
 
