ccc41883530a733f2aa8033bcc5f93430ca6c932
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-06-04 00:46:15 +0800

    Update navbar design

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     <div className="w-full flex flex-col items-center ">
       <div className="w-full flex items-center border-b-[12px] border-b-black gap-2 bg-red-500 h-fit p-3 px-3">
         <img className="w-14" src={'/pokeball.svg'} alt="" />
-        <div className="font-bold text-white text-3xl">Pokédex</div>
+        <div className="font-bold hidden md:block text-white text-3xl">Pokédex</div>
 
       </div>
       <div className="h-20 w-20 -mt-12 bg-black 
