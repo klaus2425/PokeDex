ce233da0b3c96461a455fabbfc2ac9aed6d6e8d3
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-06-04 00:45:53 +0800

    Add sorting functionality on homepage

--- src/Components/AllPokemon.tsx
+++ src/Components/AllPokemon.tsx
@@ -3,17 +3,20 @@ import { useState } from 'react'
 
 type Pokemon = {
   name: string
+  id: string
+  url: string
 }
 
 type AllPokemonProps = {
   searchKey: string;
+  sortType: string;
+  order: string
 }
 
-const AllPokemon = ({searchKey}: AllPokemonProps) => {
+const AllPokemon = ({ searchKey, sortType, order }: AllPokemonProps) => {
   const fetchData = async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=-1&order=");
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=-1");
     const pokemon = await response.json();
-    console.log(pokemon);
     return pokemon;
   }
   const [max, setMax] = useState(19);
@@ -23,25 +26,87 @@ const AllPokemon = ({searchKey}: AllPokemonProps) => {
     queryFn: fetchData,
   })
 
-
   return (
     <div className="flex flex-col items-center gap-6 p-3">
       <div className="grid w-fit  grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 auto-rows-fr gap-10 items-start">
         {
           !isLoading ?
-            data.results.map((pokemon: Pokemon, index: number) => {
-              if (index <= max) {
-                return (
-                  <div className="group border-4 border-black flex flex-col relative h-64 bg-gray-100 rounded-md items-center justify-between cursor-pointer transition duration-200 hover:bg-slate-400">
-                    <span className="mt-1 ml-1 rounded-2xl px-1 bg-red-500 font-bold self-start text-white border-2 border-black">No. {index + 1}</span>
-                    <img className="w-40 h-40 object-cover transition-transform duration-300 transform scale-100 group-hover:scale-125 absolute top-7 left-auto" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} alt={pokemon.name} />
-                    <div className="w-56 text-center bg-red-500 p-2 border-t-4 border-black">
-                      <span className="font-bold text-white uppercase">{pokemon.name}</span>
-                    </div>
-                  </div>
+            searchKey === '' ?
+              sortType === 'id' ? // Sort all Pokemon data by ID
+                data.results.sort((a: Pokemon, b: Pokemon) => order === 'ascending' ?
+                  parseInt(a.url.split('/').slice(-2, -1)[0], 10) - parseInt(b.url.split('/').slice(-2, -1)[0], 10) :
+                  parseInt(b.url.split('/').slice(-2, -1)[0], 10) - parseInt(a.url.split('/').slice(-2, -1)[0], 10)
                 )
-              }
-            })
+                  .map((pokemon: Pokemon, index: number) => {
+                    if (index <= max) {
+                      return (
+                        <div className="group border-4 border-black flex flex-col relative h-64 bg-gray-100 rounded-md items-center justify-between cursor-pointer transition duration-200 hover:bg-slate-400">
+                          <span className="mt-1 ml-1 rounded-2xl px-1 bg-red-500 font-bold self-start text-white border-2 border-black">No. {pokemon.url.split('/').slice(-2, -1)[0]}</span>
+                          <img className="w-40 h-40 object-cover transition-transform duration-300 transform scale-100 group-hover:scale-125 absolute top-7 left-auto" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/').slice(-2, -1)[0]}.png`} alt={pokemon.name} />
+                          <div className="w-52 text-center bg-red-500 p-2 border-t-4 border-black">
+                            <span className="font-bold text-white uppercase">{pokemon.name}</span>
+                          </div>
+                        </div>
+                      )
+                    }
+                  })
+                : // Sort all Pokemon data by name
+                data.results.sort((a: Pokemon, b: Pokemon) => order === 'ascending' ? a.name.localeCompare(b.name) :
+                  b.name.localeCompare(a.name)
+                )
+                  .map((pokemon: Pokemon, index: number) => {
+                    if (index <= max) {
+                      return (
+                        <div className="group border-4 border-black flex flex-col relative h-64 bg-gray-100 rounded-md items-center justify-between cursor-pointer transition duration-200 hover:bg-slate-400">
+                          <span className="mt-1 ml-1 rounded-2xl px-1 bg-red-500 font-bold self-start text-white border-2 border-black">No. {pokemon.url.split('/').slice(-2, -1)[0]}</span>
+                          <img className="w-40 h-40 object-cover transition-transform duration-300 transform scale-100 group-hover:scale-125 absolute top-7 left-auto" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/').slice(-2, -1)[0]}.png`} alt={pokemon.name} />
+                          <div className="w-52 text-center bg-red-500 p-2 border-t-4 border-black">
+                            <span className="font-bold text-white uppercase">{pokemon.name}</span>
+                          </div>
+                        </div>
+                      )
+                    }
+                  })
+              :
+              sortType === 'id' ? // Sort all searched Pokemon by ID
+                data.results
+                  .sort((a: Pokemon, b: Pokemon) => order === 'ascending' ?
+                    parseInt(a.url.split('/').slice(-2, -1)[0], 10) - parseInt(b.url.split('/').slice(-2, -1)[0], 10) :
+                    parseInt(b.url.split('/').slice(-2, -1)[0], 10) - parseInt(a.url.split('/').slice(-2, -1)[0], 10)
+                  )
+                  .map((pokemon: Pokemon) => ({ name: pokemon.name, id: pokemon.url.split('/').slice(-2, -1)[0] }))
+                  .filter((pokemon: Pokemon) => pokemon.name.toLowerCase().includes(searchKey.toLowerCase()) || pokemon.id == searchKey)
+                  .map((pokemon: Pokemon, index: number) => {
+                    if (index <= max) {
+                      return (
+                        <div className="group border-4 border-black flex flex-col relative h-64 bg-gray-100 rounded-md items-center justify-between cursor-pointer transition duration-200 hover:bg-slate-400">
+                          <span className="mt-1 ml-1 rounded-2xl px-1 bg-red-500 font-bold self-start text-white border-2 border-black">No. {pokemon.id}</span>
+                          <img className="w-40 h-40 object-cover transition-transform duration-300 transform scale-100 group-hover:scale-125 absolute top-7 left-auto" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt={pokemon.name} />
+                          <div className="w-56 text-center bg-red-500 p-2 border-t-4 border-black">
+                            <span className="font-bold text-white uppercase">{pokemon.name}</span>
+                          </div>
+                        </div>
+                      )
+                    }
+                  })
+                : // Sort all searched Pokemon by name
+                data.results.sort((a: Pokemon, b: Pokemon) => order === 'ascending' ? a.name.localeCompare(b.name) :
+                  b.name.localeCompare(a.name)
+                ).map((pokemon: Pokemon) => ({ name: pokemon.name, id: pokemon.url.split('/').slice(-2, -1)[0] }))
+                  .filter((pokemon: Pokemon) => pokemon.name.toLowerCase().includes(searchKey.toLowerCase()) || pokemon.id == searchKey)
+                  .map((pokemon: Pokemon, index: number) => {
+                    if (index <= max) {
+                      return (
+                        <div className="group border-4 border-black flex flex-col relative h-64 bg-gray-100 rounded-md items-center justify-between cursor-pointer transition duration-200 hover:bg-slate-400">
+                          <span className="mt-1 ml-1 rounded-2xl px-1 bg-red-500 font-bold self-start text-white border-2 border-black">No. {pokemon.id}</span>
+                          <img className="w-40 h-40 object-cover transition-transform duration-300 transform scale-100 group-hover:scale-125 absolute top-7 left-auto" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt={pokemon.name} />
+                          <div className="w-56 text-center bg-red-500 p-2 border-t-4 border-black">
+                            <span className="font-bold text-white uppercase">{pokemon.name}</span>
+                          </div>
+                        </div>
+                      )
+                    }
+                  })
             :
             <div>Loading...</div>
         }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,18 +1,26 @@
 import ReactSelect, { StylesConfig } from "react-select";
 import Navbar from "../Components/Navbar";
 import AllPokemon from "../Components/AllPokemon";
+import { useState } from "react";
 
 type CustomStyle = StylesConfig<{ value: string; label: string }, false>;
 
 const Home = () => {
   
   const options = [
-    { value: 'name', label: 'Name' },
     { value: 'id', label: 'Pokémon ID' },
+    { value: 'name', label: 'Name' },
   ]
 
+  const orderOptions = [
+    { value: 'ascending', label: 'Ascending' },
+    { value: 'descending', label: 'Descending' },
+  ]
 
-
+  const [searchKey, setSearchKey] = useState('');
+  const [sortType, setSortType] = useState('id');
+  const [inputValue, setInputValue] = useState('');
+  const [order, setOrder] =  useState('ascending');
   const customStyles:CustomStyle = {
     container: (provided) => ({ ...provided, width: '10rem' }),
     option: (provided, state) => ({
@@ -22,28 +30,57 @@ const Home = () => {
     }),
   };
 
-
+  const handleSearch = () => {
+    setSearchKey(inputValue);
+  }
 
   return (
     <div className="flex flex-col gap-4 items-center">
       <Navbar />
       <div className="text-4xl mt-4 font-bold">Pokémon</div>
       <div className="w-3/12 min-w-72 flex items-center gap-1">
-        <input className="rounded px-1 p-0.5 w-full border-2 outline-gray-500" type="text" placeholder="Search Pokémon" />
-        🔍
+        <input onChange={(ev) => setInputValue(ev.currentTarget.value)} className="rounded px-1 p-0.5 w-full border-2 outline-gray-500" 
+        type="text" 
+        placeholder="Search Pokémon" 
+        onKeyDown={event => {
+          if (event.key === 'Enter') {
+            handleSearch();
+          }
+        }}
+        />
+        <button onClick={handleSearch}>🔍</button> 
       </div>
       <div className="flex justify-center items-center gap-3">
-        <div>Sort by: </div>
+        <div className="font-bold">Sort by: </div>
         <div>
           <ReactSelect
             options={options}
             styles={customStyles}
-            defaultValue={{ value: 'name', label: 'Name' }}
+            defaultValue={{ value: 'id', label: 'Pokemon ID' }}
             isSearchable={false}
+            onChange={(value) => {
+              if (value && value.value) { 
+                setSortType(value.value);
+              }
+            }}
+            />
+        </div>
+        <div className="font-bold">Order by: </div>
+        <div>
+          <ReactSelect
+            options={orderOptions}
+            styles={customStyles}
+            defaultValue={{ value: 'Ascending', label: 'Ascending' }}
+            isSearchable={false}
+            onChange={(value) => {
+              if (value && value.value) { 
+                setOrder(value.value);
+              }
+            }}
             />
         </div>
       </div>
-      <AllPokemon searchKey={''}/>
+      <AllPokemon searchKey={searchKey} sortType={sortType} order={order}/>
     </div>
   )
 }
