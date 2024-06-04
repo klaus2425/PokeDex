9b7082cf4baf4d55de9715540a2a3a904516f622
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-06-04 17:43:01 +0800

    Add fetching of all pokemon data to DefaultLayout

--- src/Layouts/DefaultLayout.tsx
+++ src/Layouts/DefaultLayout.tsx
@@ -1,7 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
 import Navbar from '../Components/Navbar'
 import { Outlet } from 'react-router-dom'
+import { useStateContext } from '../Context/ContextProvider';
 
 const DefaultLayout = () => {
+  const {setResults} = useStateContext();
+
+  const fetchData = async () => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=-1");
+    const pokemon = await response.json();
+    setResults(pokemon.results)
+    return pokemon;
+  }
+
+  const pokemon = useQuery({
+    queryKey: ['all-pokemon-data'],
+    queryFn: fetchData,
+  })
+
   return (
     <div>
       <Navbar />
