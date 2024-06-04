736c0762259fb226b12356ac2d190c29e2c89e10
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-06-04 11:41:03 +0800

    Add stats section on detailed Pokemon view

---
+++ src/Components/Progressbar.tsx
@@ -0,0 +1,18 @@
+type props = {
+  value: number;
+  max: number;
+}
+
+const Progressbar = ({value, max}:props) => {
+  const percentage = (value / max) * 100;
+  return (
+    <div className='w-full bg-gray-200 rounded-full h-4 overflow-hidden'>
+      <div
+        className="bg-green-500 h-full transition-all duration-500"
+        style={{ width: `${percentage}%` }}
+      ></div>
+    </div>
+  )
+}
+
+export default Progressbar
--- src/Pages/Pokemon.tsx
+++ src/Pages/Pokemon.tsx
@@ -1,6 +1,14 @@
 import { useQuery } from "@tanstack/react-query"
-import Navbar from "../Components/Navbar"
 import { useParams } from "react-router-dom"
+import Progressbar from "../Components/Progressbar";
+
+
+type Stat = {
+  base_stat: number;
+  stat: {
+    name: string
+  };
+}
 
 const Pokemon = () => {
   const { name, id } = useParams();
@@ -26,20 +34,32 @@ const Pokemon = () => {
     queryKey: [`${name}-description`],
     queryFn: fetchDescription,
   })
-
   console.log(pokemon.data);
-  return pokemon.data ? (
-    <div className="flex flex-col gap-3">
-      <Navbar />
+
+  return pokemon.data && pokemon_description.data ? (
+    <div className="flex mt-6 flex-col gap-3">
       <div className="w-full flex flex-col items-center">
         <div className="bg-gray-200 rounded-md w-4/6 min-w-72 max-w-4xl">
-          <div className="w-full text-center">
-            <span>{pokemon.data.name.toUpperCase()} #{pokemon.data.id}</span>
+          <div className="w-full text-center pt-3 mb-3">
+            <span className="font-bold text-3xl">{pokemon.data.name.toUpperCase()} #{pokemon.data.id}</span>
           </div>
-          <div className="w-full flex justify-center">
+          <div className="w-full flex justify-center gap-6">
             <img className="w-40 h-40 object-cover transition-transform duration-300 transform scale-100 hover:scale-125 top-7 left-auto" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.data.id}.png`} alt={pokemon.data.name} />
-
+            <div className="w-1/3 flex flex-col gap-3">
+              <span>{pokemon_description.data.flavor_text_entries[0].flavor_text.replace('\f', ' ')}</span>
+              <div className="w-full bg-red-500 p-3 rounded-lg">
+                <span className="font-bold text-white">Stats</span>
+                {
+                  pokemon.data.stats.map((stat: Stat, index: number) => (
+                    <div>
+                      <span className="text-white p-2 font-bold">{stat.stat.name.toUpperCase()}</span> <Progressbar value={pokemon.data.stats[index].base_stat} max={255} />
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
           </div>
+
         </div>
       </div>
     </div>
