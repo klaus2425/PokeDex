af4d77dd608cb107f183259db66be16e9566c4d7
Jaycie Dela Cruz <102586901+klaus2425@users.noreply.github.com>
2024-06-04 16:36:41 +0800

    Create loader

---
+++ src/Components/Loading.tsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+  return (
+    <div className="mt-80 flex justify-center items-center">
+      <img className="w-1/12 animate-bounce min-w-36" src={`/pokeball.svg`} alt="Loading" />
+    </div>
+  )
+}
+
+export default Loading
